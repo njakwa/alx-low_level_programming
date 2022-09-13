4adcb05e8a1fa0d1673aa4a3a69da3c16f16b095
njakwa <greychakhalira@gmail.com>
2022-09-13 09:38:19 -0700

    alpha

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,15 +1,14 @@
 #include "main.h"
 
 /**
- *  *_islower - checks for a lowercase character
+ *  * _isalpha - check the code for Holberton School students.
  *   *@c: letter being tested
  *    * Return: Always 0
  *     */
 
-int _islower(int c)
+int _isalpha(int c)
 {
-
-		if (c >= 97 && c <= 122)
+		if ((c >= 65 && c <= 90) || (c >= 97 && c <= 122))
 					return (1);
 			else
 						return (0);
