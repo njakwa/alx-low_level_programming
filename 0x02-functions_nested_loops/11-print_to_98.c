b03c97bfb4d9a5f3000c29908e45419192ae19b3
njakwa <greychakhalira@gmail.com>
2022-09-13 09:15:15 -0700

    eleven

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -8,13 +8,13 @@
 
 void print_to_98(int n)
 {
-		if (n <= 98)
-				{
-							for (n = n; n <= 97; n++)
-											printf("%d, ", n);
+	if (n <= 98)
+		{
+			for (n = n; n <= 97; n++)
+				printf("%d, ", n);
 								}
 			else
-						for (n = n; n > 98; n--)
-										printf("%d, ", n);
-				printf("98\n");
+				for (n = n; n > 98; n--)
+					printf("%d, ", n);
+			printf("98\n");
 }
