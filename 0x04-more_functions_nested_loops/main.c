96c64aa796865c4aed4f919fa50c5c97c2bc106b
njakwa <greychakhalira@gmail.com>
2022-09-15 11:09:35 -0700

    done

--- 0x04-more_functions_nested_loops/main.c
+++ 0x04-more_functions_nested_loops/main.c
@@ -1,18 +1,27 @@
-#include "main.h"
-#include <stdio.h>
+#ifndef _MAIN_H_
+#define _MAIN_H_
 
-/**
- *  * main - check the code for Holberton School students.
- *   *
- *    * Return: Always 0.
- *     */
-int main(void)
-{
-		char c;
+int _putchar(char c);
+void print_alphabet(void);
+void print_alphabet_x10(void);
+int _islower(int c);
+int _isalpha(int c);
+int print_sign(int n);
+int _abs(int);
+int print_last_digit(int);
+void jack_bauer(void);
+void times_table(void);
+int add(int, int);
+void print_to_98(int n);
+int _isupper(int c);
+int _isdigit(int c);
+int mul(int a, int b);
+void print_numbers(void);
+void print_most_numbers(void);
+void more_numbers(void);
+void print_line(int n);
+void print_diagonal(int n);
+void print_square(int size);
+void print_triangle(int size);
 
-			c = 'A';
-				printf("%c: %d\n", c, _isupper(c));
-					c = 'a';
-						printf("%c: %d\n", c, _isupper(c));
-							return (0);
-}
+#endif 
