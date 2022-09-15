b2de5ccec0200a68b2ede29d5396120a4334ccfc
njakwa <84509198+njakwa@users.noreply.github.com>
2022-09-15 20:16:35 +0100

    Update 6-print_line.c

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,17 +1,17 @@
 #include "main.h"
 
 /**
- *  * print_line - draws a straight line in the terminal
- *   *@n: number of times to print the line
- *    * Return: Always 0.
- *     */
+ * print_line - draws a straight line in the terminal
+ *@n: number of times to print the line
+ * Return: Always 0.
+ */
 
 void print_line(int n)
 {
-		while (n > 0)
-				{
-							_putchar('_');
-									n--;
-										}
-			_putchar('\n');
+	while (n > 0)
+	{
+		_putchar('_');
+		n--;
+	}
+	_putchar('\n');
 }
