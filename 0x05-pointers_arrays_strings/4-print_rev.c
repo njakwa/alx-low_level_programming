155424701721bf7aab2d4acd93c0923f3d7f3d09
njakwa <84509198+njakwa@users.noreply.github.com>
2022-09-19 19:25:52 +0100

    Update 4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,22 +1,16 @@
-#include "holberton.h"
+#include "main.h"
 
-/**
- *  * print_rev - prints a string in stdout in reverse
- *   * @s: string to print
- *    *
- *     * Return: void
- *      */
 void print_rev(char *s)
 {
-		int i = 0;
+	int i = 0;
 
-			while (*(s + i))
-						i++;
-				i = i - 1;
-					while (i >= 0)
-							{
-										_putchar(*(s + i));
-												i--;
-													}
-						_putchar('\n');
+	while (*(s + i))
+		i++;
+	i = i - 1;
+	while (i >= 0)
+	{
+		_putchar(*(s + i));
+		i--;
+	}
+	_putchar('\n');
 }
