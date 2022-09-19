cfafa3263c44b531eb1876e2716fa3567604d09f
njakwa <84509198+njakwa@users.noreply.github.com>
2022-09-19 20:10:59 +0100

    Update 8-print_array.c

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -2,22 +2,22 @@
 #include <stdio.h>
 
 /**
- *  * print_array - prints n elements in the array a
- *   * @a: array to print
- *    * @n: number of elements to print
- *     *
- *      * Return: void
- *       */
+ * print_array - prints n elements in the array a
+ * @a: array to print
+ * @n: number of elements to print
+ *
+ * Return: void
+ */
 void print_array(int *a, int n)
 {
-		int i = 0;
+	int i = 0;
 
-			while (i < n)
-					{
-								printf("%d", a[i]);
-										if (i < (n - 1))
-														printf(", ");
-												i++;
-													}
-				printf("\n");
+	while (i < n)
+	{
+		printf("%d", a[i]);
+		if (i < (n - 1))
+			printf(", ");
+		i++;
+	}
+	printf("\n");
 }
